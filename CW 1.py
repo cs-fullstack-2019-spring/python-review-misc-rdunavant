003144bccccf3041b1472d814cfbc12a96ec5e44
rdunavant0 <rdunavant0@gmail.com>
2019-01-23 11:50:42 -0600

    message

---
+++ CW 1.py
@@ -0,0 +1,48 @@
+Problem 1
+def main():
+    userInput = input("Enter something")
+    for ("q" or "Q")
+    if not "q" or "Q":
+    userInput2 = input("Enter something else")
+
+Problem 2
+def main():
+    exercise2():
+    exercise2_helper(num1, num2, num3):
+    num1 = str(3)
+    num2 = str(12)
+    num3 = str(23)
+    exercise2_helper(3,12,23, "SUM")
+    exercise2_helper(3,12,23, "PROD")
+    exercise2_helper(3,12,23, "AVG")
+    print(sum)
+    print(prod)
+    print(avg)
+    if not(switch/case):
+        print("invalid operation")
+
+Challenge 1
+def main():
+    num = input("How many stars should I print?")
+    for (i=1)
+    (1<=num)
+    if rows = 5:
+        print(*
+              **
+              ***
+              ****
+              *****)
+
+Challenge2
+def main():
+    speed = input("How fast was the driver going")
+    if speed < 70:
+        print("Ok")
+    else
+    for 5++ > 70
+    userInput = input("Give driver one demerit point")
+    print(userInput)
+    if speed == 80:
+        print("Points: 2")
+    if userInput > 12:
+        print("License Suspended")
